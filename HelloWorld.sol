8f2eb80edaac1036da2ae8b038ce5ee1c16e9680
davidtnfsh <davidtnfsh@gmail.com>
2020-09-11 22:10:08 +0800

    style: 💄 add linter and formatter

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -2,14 +2,14 @@
 pragma solidity ^0.7.1;
 
 contract HelloWorld {
-  string myName = "Tristan";
-  
-  // Add this function:
-  function getMyName() public view returns(string memory) {
-    return myName;
-  }
-  
-  function changeMyName(string memory _newName) public {
-    myName = _newName;
-  }
+    string myName = "Tristan";
+
+    // Add this function:
+    function getMyName() public view returns (string memory) {
+        return myName;
+    }
+
+    function changeMyName(string memory _newName) public {
+        myName = _newName;
+    }
 }
